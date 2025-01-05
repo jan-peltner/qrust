70bb75815450b49ccd42b34f697db39f36407593
jan <defPelly@proton.me>
2025-01-05 15:27:26 +0100

    FEAT: display workspace infos in header

--- src/ui.rs
+++ src/ui.rs
@@ -50,15 +50,30 @@ fn render_app_scaffold(viewport: Rc<[Rect]>, frame: &mut Frame, app: &AppState) 
         ])
         .split(header);
 
-    let title_center_block = Block::default().style(Style::default().bg(Color::White));
-    let title_center = Paragraph::new(Span::raw("Postr").red().bold().underlined())
-        .block(title_center_block)
+    let title_top_block = Block::default().style(Style::default().bg(Color::White));
+    let title_top = Paragraph::new(Span::raw("Postr").red().bold().underlined())
+        .block(title_top_block)
         .alignment(Alignment::Center);
-    let title_top = Block::default().on_white();
-    let title_bottom = Block::default().on_white();
 
     frame.render_widget(title_top, title_chunks[0]);
+
+    let title_center_block = Block::default().style(Style::default().bg(Color::White));
+    let title_center_line = Line::from(Vec::from([Span::raw("Workspace: "), Span::raw(&app.name)]));
+    let title_center = Paragraph::new(title_center_line.red())
+        .block(title_center_block)
+        .alignment(Alignment::Center);
+
     frame.render_widget(title_center, title_chunks[1]);
+
+    let title_bottom_block = Block::default().style(Style::default().bg(Color::White));
+    let title_bottom_line = Line::from(Vec::from([
+        Span::raw("Endpoint: "),
+        Span::raw(app.query.url.host_str().unwrap()),
+    ]));
+    let title_bottom = Paragraph::new(title_bottom_line.red())
+        .block(title_bottom_block)
+        .alignment(Alignment::Center);
+
     frame.render_widget(title_bottom, title_chunks[2]);
 
     let footer_chunks = Layout::default()
