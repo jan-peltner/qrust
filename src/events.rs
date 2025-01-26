bb1470fe2f14d496c5791eceb996b49f45c048f3
jan <jan.peltner@ggmgastro.com>
2025-01-26 23:16:38 +0100

    chore: make event loop non-blocking Use the crossterms `poll` API to check whether a keyboard event is available to read from. This makes the app loop non-blocking and drives any open network requests to completion.

--- src/events.rs
+++ src/events.rs
@@ -1,4 +1,4 @@
-use std::{future::Future, pin::Pin};
+use std::{future::Future, pin::Pin, time::Duration};
 
 use ratatui::crossterm::event::{self, Event, KeyCode, KeyEventKind, KeyModifiers};
 use reqwest::Response;
@@ -9,27 +9,38 @@ pub fn handle_events(
     app: &mut AppState<'_>,
     client: &GqlClient<'_>,
 ) -> Option<Pin<Box<dyn Future<Output = Result<Response, reqwest::Error>>>>> {
-    if let Ok(Event::Key(key)) = event::read() {
-        if key.kind == KeyEventKind::Release {
-            return None;
+    if is_event_available() {
+        if let Ok(Event::Key(key)) = event::read() {
+            if key.kind == KeyEventKind::Release {
+                return None;
+            }
+            match key.code {
+                KeyCode::Char(char) => {
+                    if key.modifiers == KeyModifiers::CONTROL && (char == 'q' || char == 'c') {
+                        app.should_quit = true;
+                    }
+                }
+                KeyCode::Backspace => {}
+                KeyCode::Tab => {
+                    app.handle_focus_transition();
+                }
+                KeyCode::Enter => {
+                    if let Ok(request) = client.build_request(app.query.as_str()) {
+                        return Some(Box::pin(request.send()));
+                    }
+                }
+                _ => {}
+            };
         }
-        match key.code {
-            KeyCode::Char(char) => {
-                if key.modifiers == KeyModifiers::CONTROL && (char == 'q' || char == 'c') {
-                    app.should_quit = true;
-                }
-            }
-            KeyCode::Backspace => {}
-            KeyCode::Tab => {
-                app.handle_focus_transition();
-            }
-            KeyCode::Enter => {
-                if let Ok(request) = client.build_request(app.query.as_str()) {
-                    return Some(Box::pin(request.send()));
-                }
-            }
-            _ => {}
-        };
     }
+
     return None;
+}
+
+fn is_event_available() -> bool {
+    if let Ok(is_available) = event::poll(Duration::from_millis(100)) {
+        is_available
+    } else {
+        false
+    }
 }
